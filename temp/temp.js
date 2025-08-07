112df16e05a43b5f9b5a3997fcfebd4c861c6cac
sergio <sergii.kapustin@gmail.com>
2025-08-07 17:07:22 +0300

    Додав файл temp/temp.js з тестовим JavaScript кодом

---
+++ temp/temp.js
@@ -0,0 +1,11 @@
+// Тестовий JavaScript файл
+// test comment
+console.log('Hello, World!');
+
+function greetUser(name) {
+    return `Привіт, ${name}! Ласкаво просимо до нашого проєкту.`;
+}
+
+// Приклад використання
+const userName = 'DevOps студент';
+console.log(greetUser(userName));
