ba8f42a2d1f2e9d3c0587b5ec19b5c56a6f86f50
sergio <sergii.kapustin@gmail.com>
2025-08-23 14:28:05 +0300

    Add Terraform modules for S3, VPC, and ECR

--- lesson-5/backend.tf
+++ lesson-5/backend.tf
@@ -1,9 +1,9 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-state-bucket-lesson5-sergio-2025"
-    key            = "lesson-5/terraform.tfstate"
-    region         = "us-west-2"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket         = "terraform-state-bucket-lesson5-sergio-2025"
+#     key            = "lesson-5/terraform.tfstate"
+#     region         = "us-west-2"
+#     dynamodb_table = "terraform-locks"
+#     encrypt        = true
+#   }
+# }
