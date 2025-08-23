97fe92a1333a3c0d085ac421e8f98c87a915d79b
sergio <sergii.kapustin@gmail.com>
2025-08-23 14:00:40 +0300

    Add Terraform modules for S3, VPC, and ECR

---
+++ lesson-5/modules/s3-backend/variables.tf
@@ -0,0 +1,26 @@
+variable "bucket_name" {
+  description = "Name of the S3 bucket for Terraform state"
+  type        = string
+}
+
+variable "table_name" {
+  description = "Name of the DynamoDB table for state locking"
+  type        = string
+  default     = "terraform-locks"
+}
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-west-2"
+}
+
+variable "tags" {
+  description = "Tags to apply to resources"
+  type        = map(string)
+  default = {
+    Project     = "lesson-5"
+    Environment = "dev"
+    ManagedBy   = "terraform"
+  }
+}
